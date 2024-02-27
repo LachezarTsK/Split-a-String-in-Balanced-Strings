3a4cb270be8d4d208ce0f0d560a6d6fbb80f64f6
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-02-27 20:57:00 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,20 @@
+
+#include <string>
+using namespace std;
+
+class Solution {
+
+    inline static const array<char, 2> CHARACTERS_IN_INPUT { {'L', 'R'} };
+
+public:
+    int balancedStringSplit(const string& input) const {
+        int maxNumberOfBalancedSubstrings = 0;
+        int currentBalance = 0;
+
+        for (const auto& letter : input) {
+            currentBalance += (letter == CHARACTERS_IN_INPUT[0]) ? 1 : -1;
+            maxNumberOfBalancedSubstrings += (currentBalance == 0) ? 1 : 0;
+        }
+        return maxNumberOfBalancedSubstrings;
+    }
+};
